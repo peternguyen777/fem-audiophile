eea21f4438b63b696aa7d952f4fbbcfa2124bb2b
peternguyen777 <peter.quang.nguyen@gmail.com>
2022-08-08 12:39:36 +1000

    mobile menu test

--- components/UI/MobileMenu.js
+++ components/UI/MobileMenu.js
@@ -15,82 +15,80 @@ export default function MobileMenu({ mobMenuOpen, setMobMenuOpen }) {
   }, []);
 
   const modalContent = (
-    <>
-      <div
-        className={`absolute top-[90px] z-30 h-fit w-full select-none rounded-b-lg bg-lightgray px-6 pb-[35px] pt-[84px] md:hidden ${
-          mobMenuOpen ? "translate-y-0" : "-translate-y-full"
-        } duration-300 ease-in-out`}
-      >
-        <section className='flex flex-col space-y-[68px]'>
-          <div className='relative flex flex-col items-center rounded-lg bg-gray pb-[22px]'>
-            <Image
-              src={headphonesThumb}
-              alt=''
-              className='absolute h-[140px] -translate-y-[38%] object-contain'
-            />
-            <h6 className='mobmenu-h mt-[88px]'>HEADPHONES</h6>
-            <Link href='/'>
-              <div className='mt-[17px] flex cursor-pointer items-center'>
-                <p className='mobmenu-p mr-[13px]'>SHOP</p>
-                <svg width='8' height='12' xmlns='http://www.w3.org/2000/svg'>
-                  <path
-                    d='M1.322 1l5 5-5 5'
-                    stroke='#D87D4A'
-                    strokeWidth='2'
-                    fill='none'
-                    fillRule='evenodd'
-                  />
-                </svg>
-              </div>
-            </Link>
-          </div>
-          <div className='relative flex flex-col items-center rounded-lg bg-gray pb-[22px]'>
-            <Image
-              src={speakersThumb}
-              alt=''
-              className='absolute h-[140px] -translate-y-[40%] object-contain'
-            />
-            <h6 className='mobmenu-h mt-[88px]'>SPEAKERS</h6>
-            <Link href='/'>
-              <div className='mt-[17px] flex cursor-pointer items-center'>
-                <p className='mobmenu-p mr-[13px]'>SHOP</p>
-                <svg width='8' height='12' xmlns='http://www.w3.org/2000/svg'>
-                  <path
-                    d='M1.322 1l5 5-5 5'
-                    stroke='#D87D4A'
-                    strokeWidth='2'
-                    fill='none'
-                    fillRule='evenodd'
-                  />
-                </svg>
-              </div>
-            </Link>
-          </div>
-          <div className='relative flex flex-col items-center rounded-lg bg-gray pb-[22px]'>
-            <Image
-              src={earphonesThumb}
-              alt=''
-              className='absolute h-[140px] -translate-y-[30%] object-contain'
-            />
-            <h6 className='mobmenu-h mt-[88px]'>EARPHONES</h6>
-            <Link href='/'>
-              <div className='mt-[17px] flex cursor-pointer items-center'>
-                <p className='mobmenu-p mr-[13px]'>SHOP</p>
-                <svg width='8' height='12' xmlns='http://www.w3.org/2000/svg'>
-                  <path
-                    d='M1.322 1l5 5-5 5'
-                    stroke='#D87D4A'
-                    strokeWidth='2'
-                    fill='none'
-                    fillRule='evenodd'
-                  />
-                </svg>
-              </div>
-            </Link>
-          </div>
-        </section>
-      </div>
-    </>
+    <div
+      className={`mobMenuScroll fixed top-[90px] z-30 h-[calc(100vh-90px)] max-h-[750px] w-full select-none overflow-y-scroll rounded-b-lg bg-lightgray px-6 pb-[35px] pt-[84px] duration-300 ease-in-out md:hidden ${
+        mobMenuOpen ? "translate-y-0" : "-translate-y-full"
+      }`}
+    >
+      <section className='flex flex-col space-y-[68px]'>
+        <div className='relative flex flex-col items-center rounded-lg bg-gray pb-[22px]'>
+          <Image
+            src={headphonesThumb}
+            alt=''
+            className='absolute h-[140px] -translate-y-[38%] object-contain'
+          />
+          <h6 className='mobmenu-h mt-[88px]'>HEADPHONES</h6>
+          <Link href='/'>
+            <div className='mt-[17px] flex cursor-pointer items-center'>
+              <p className='mobmenu-p mr-[13px]'>SHOP</p>
+              <svg width='8' height='12' xmlns='http://www.w3.org/2000/svg'>
+                <path
+                  d='M1.322 1l5 5-5 5'
+                  stroke='#D87D4A'
+                  strokeWidth='2'
+                  fill='none'
+                  fillRule='evenodd'
+                />
+              </svg>
+            </div>
+          </Link>
+        </div>
+        <div className='relative flex flex-col items-center rounded-lg bg-gray pb-[22px]'>
+          <Image
+            src={speakersThumb}
+            alt=''
+            className='absolute h-[140px] -translate-y-[40%] object-contain'
+          />
+          <h6 className='mobmenu-h mt-[88px]'>SPEAKERS</h6>
+          <Link href='/'>
+            <div className='mt-[17px] flex cursor-pointer items-center'>
+              <p className='mobmenu-p mr-[13px]'>SHOP</p>
+              <svg width='8' height='12' xmlns='http://www.w3.org/2000/svg'>
+                <path
+                  d='M1.322 1l5 5-5 5'
+                  stroke='#D87D4A'
+                  strokeWidth='2'
+                  fill='none'
+                  fillRule='evenodd'
+                />
+              </svg>
+            </div>
+          </Link>
+        </div>
+        <div className='relative flex flex-col items-center rounded-lg bg-gray pb-[22px]'>
+          <Image
+            src={earphonesThumb}
+            alt=''
+            className='absolute h-[140px] -translate-y-[30%] object-contain'
+          />
+          <h6 className='mobmenu-h mt-[88px]'>EARPHONES</h6>
+          <Link href='/'>
+            <div className='mt-[17px] flex cursor-pointer items-center'>
+              <p className='mobmenu-p mr-[13px]'>SHOP</p>
+              <svg width='8' height='12' xmlns='http://www.w3.org/2000/svg'>
+                <path
+                  d='M1.322 1l5 5-5 5'
+                  stroke='#D87D4A'
+                  strokeWidth='2'
+                  fill='none'
+                  fillRule='evenodd'
+                />
+              </svg>
+            </div>
+          </Link>
+        </div>
+      </section>
+    </div>
   );
 
   const underlayContent = (
@@ -126,4 +124,12 @@ export default function MobileMenu({ mobMenuOpen, setMobMenuOpen }) {
   } else {
     return null;
   }
+}
+
+{
+  /* <div
+className={`absolute top-[90px] z-30 h-fit w-full select-none rounded-b-lg bg-lightgray px-6 pb-[35px] pt-[84px] md:hidden ${
+  mobMenuOpen ? "translate-y-0" : "-translate-y-full"
+} duration-300 ease-in-out`}
+> */
 }
